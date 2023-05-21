47f32e0ba59d8a61dc095983121156d4f62170f9
The Stig <TheStig294@users.noreply.github.com>
2023-05-21 19:44:42 +0930

    Changed holstered PaP to a self-PaP

--- gamemodes/terrortown/entities/weapons/weapon_ttt_unarmed_pap.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_unarmed_pap.lua
@@ -1,17 +1,15 @@
 AddCSLuaFile()
-SWEP.HoldType = "fist"
+SWEP.HoldType = "normal"
 
 if CLIENT then
-    SWEP.PrintName = "Fists"
+    SWEP.PrintName = "Self-PaP"
     SWEP.Slot = 5
     SWEP.ViewModelFOV = 10
 end
 
 SWEP.Base = "weapon_tttbase"
-SWEP.ViewModel = "models/weapons/c_arms.mdl"
+SWEP.ViewModel = "models/weapons/v_crowbar.mdl"
 SWEP.WorldModel = "models/weapons/w_crowbar.mdl"
-SWEP.UseHands = true
-SWEP.Primary.Damage = 30
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = false
@@ -27,14 +25,40 @@ SWEP.InLoadoutFor = {ROLE_INNOCENT, ROLE_TRAITOR, ROLE_DETECTIVE}
 SWEP.AllowDelete = false
 SWEP.AllowDrop = false
 SWEP.NoSights = true
-local swingSound = Sound("Weapon_Crowbar.Single")
-
-function SWEP:Initialize()
-    self:SetHoldType(self.HoldType)
-end
+SWEP.BuffScale = 1.3
 
 function SWEP:GetClass()
     return "weapon_ttt_unarmed_pap"
+end
+
+function SWEP:Initialize()
+    local owner = self:GetOwner()
+
+    if IsValid(owner) and owner:IsPlayer() then
+        self.HolsterPAPOwner = owner
+        owner:ChatPrint("You pack-a-punched yourself!")
+        owner:SetMaterial(TTT_PAP_CAMO)
+        owner:SetFOV(0)
+        owner:SetFOV(owner:GetFOV() * self.BuffScale)
+        owner:SetLaggedMovementValue(owner:GetLaggedMovementValue() * self.BuffScale)
+        owner:SetJumpPower(owner:GetJumpPower() * self.BuffScale)
+        owner:SetHealth(owner:Health() * self.BuffScale)
+        owner:SetMaxHealth(owner:GetMaxHealth() * self.BuffScale)
+    end
+end
+
+function SWEP:OnRemove()
+    local owner = self.HolsterPAPOwner
+
+    if IsValid(owner) and owner:IsPlayer() then
+        owner:ChatPrint("Your pack-a-punch buff has been removed")
+        owner:SetMaterial("")
+        owner:SetFOV(0)
+        owner:SetLaggedMovementValue(owner:GetLaggedMovementValue() / self.BuffScale)
+        owner:SetJumpPower(owner:GetJumpPower() / self.BuffScale)
+        owner:SetHealth(owner:Health() / self.BuffScale)
+        owner:SetMaxHealth(owner:GetMaxHealth() / self.BuffScale)
+    end
 end
 
 function SWEP:OnDrop()
@@ -46,16 +70,6 @@ function SWEP:ShouldDropOnDie()
 end
 
 function SWEP:PrimaryAttack()
-    local owner = self:GetOwner()
-
-    if IsValid(owner) and owner:IsPlayer() then
-        owner:SetAnimation(PLAYER_ATTACK1)
-        self:EmitSound(swingSound)
-
-        local animations = {ACT_VM_SWINGMISS, ACT_VM_SWINGHARD, ACT_VM_SWINGHIT}
-
-        self:SendWeaponAnim(animations[math.random(1, #animations)])
-    end
 end
 
 function SWEP:SecondaryAttack()
@@ -65,9 +79,21 @@ function SWEP:Reload()
 end
 
 function SWEP:Deploy()
+    if SERVER and IsValid(self:GetOwner()) then
+        self:GetOwner():DrawViewModel(false)
+    end
+
+    self:DrawShadow(false)
+
     return true
 end
 
 function SWEP:Holster()
     return true
+end
+
+function SWEP:DrawWorldModel()
+end
+
+function SWEP:DrawWorldModelTranslucent()
 end
