258a15bfbe32ac0f770f4037121e47b767982562
The Stig <TheStig294@users.noreply.github.com>
2023-07-21 12:39:45 +0930

    Cleanup car gun to make way for full implementation

--- gamemodes/terrortown/entities/entities/ent_ttt_car_gun_pap.lua
+++ gamemodes/terrortown/entities/entities/ent_ttt_car_gun_pap.lua
@@ -1,40 +1,39 @@
 AddCSLuaFile()
 
-local targetDamageCvar = CreateConVar("ttt_car_gun_target_damage", "10000", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much damage the target receives, not players in the way of the car", 0)
+local targetDamageCvar = CreateConVar("ttt_pap_car_gun_target_damage", "10000", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much damage the target receives, not players in the way of the truck", 0)
 
-local nonTargetDamageCvar = CreateConVar("ttt_car_gun_non_target_damage", "50", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much damage players in the way of the car receive, not the target", 0)
+local nonTargetDamageCvar = CreateConVar("ttt_pap_car_gun_non_target_damage", "50", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much damage players in the way of the truck receive, not the target", 0)
 
-local speedCvar = CreateConVar("ttt_car_gun_speed", "150", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Speed of the car prop", 1)
+local speedCvar = CreateConVar("ttt_pap_car_gun_speed", "150", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Speed of the truck prop", 1)
 
-local scaleCvar = CreateConVar("ttt_car_gun_scale", "0.75", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Size scale of the car prop", 0.3)
+local scaleCvar = CreateConVar("ttt_pap_car_gun_scale", "0.75", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Size scale of the truck prop", 0.3)
 
-local rangeCvar = CreateConVar("ttt_car_gun_range", "4000", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Range of the car before it automatically disappears", 10)
+local rangeCvar = CreateConVar("ttt_pap_car_gun_range", "4000", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Range of the truck before it automatically disappears", 10)
 
 ENT.Base = "base_anim"
 ENT.Type = "anim"
 ENT.PrintName = "Truck"
 ENT.AutomaticFrameAdvance = true
 ENT.RenderGroup = RENDERGROUP_OPAQUE
-ENT.Sound = Sound("ttt_car_gun/mycar.mp3")
-ENT.CarTargetDamage = targetDamageCvar:GetInt()
-ENT.CarDamage = nonTargetDamageCvar:GetInt()
-ENT.CarSpeed = speedCvar:GetInt()
-ENT.CarScale = scaleCvar:GetFloat()
-ENT.CarRange = rangeCvar:GetInt()
+ENT.Sound = Sound("ttt_truck_gun/mytruck.mp3")
+ENT.TruckTargetDamage = targetDamageCvar:GetInt()
+ENT.TruckDamage = nonTargetDamageCvar:GetInt()
+ENT.TruckSpeed = speedCvar:GetInt()
+ENT.TruckScale = scaleCvar:GetFloat()
+ENT.TruckRange = rangeCvar:GetInt()
 ENT.ToRemove = false
 ENT.Dist = 0
 ENT.Target = nil
 
-local carModels = {"models/props_vehicles/car001a_hatchback.mdl", "models/props_vehicles/car001b_hatchback.mdl", "models/props_vehicles/car002a.mdl", "models/props_vehicles/car002b.mdl", "models/props_vehicles/car003a.mdl", "models/props_vehicles/car003b.mdl", "models/props_vehicles/car004a.mdl", "models/props_vehicles/car004b.mdl", "models/props_vehicles/car005a.mdl", "models/props_vehicles/car005b.mdl"}
-
 function ENT:Initialize()
-    self.CarTargetDamage = targetDamageCvar:GetInt()
-    self.CarDamage = nonTargetDamageCvar:GetInt()
-    self.CarSpeed = speedCvar:GetInt()
-    self.CarScale = scaleCvar:GetFloat()
-    self.CarRange = rangeCvar:GetInt()
+    self.TruckTargetDamage = targetDamageCvar:GetInt()
+    self.TruckDamage = nonTargetDamageCvar:GetInt()
+    self.TruckSpeed = speedCvar:GetInt()
+    self.TruckScale = scaleCvar:GetFloat()
+    self.TruckRange = rangeCvar:GetInt()
     self:SetModel("models/ttt_pack_a_punch/semitruck/semitruck.mdl")
-    self:SetModelScale(self.CarScale)
+    self:SetMaterial("models/ttt_pack_a_punch/semitruck/semitruck")
+    self:SetModelScale(self.TruckScale)
     self:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE_DEBRIS)
     self:SetSolid(SOLID_OBB)
 
@@ -58,10 +57,10 @@ function ENT:Think()
     local pos = self:GetPos()
     local ang = self:GetAngles()
     ang:RotateAroundAxis(ang:Up(), 2)
-    local forward = ang:Forward() * self.CarSpeed * deltaTime
+    local forward = ang:Forward() * self.TruckSpeed * deltaTime
     self.Dist = self.Dist + forward:Length()
 
-    if self.Dist > self.CarRange then
+    if self.Dist > self.TruckRange then
         self.ToRemove = true
     end
 
@@ -92,7 +91,7 @@ function ENT:StartTouch(ent)
     if ent == owner then return end
 
     if ent:Alive() and not ent:IsSpec() then
-        local inflictWep = ents.Create("weapon_ttt_car_gun")
+        local inflictWep = ents.Create("weapon_ttt_truck_gun")
         local dmg = DamageInfo()
         dmg:SetInflictor(inflictWep)
 
@@ -105,7 +104,7 @@ function ENT:StartTouch(ent)
         -- Target is insta-killed, other players in the way take damage
         if ent == self.Target then
             ent:UnLock()
-            dmg:SetDamage(self.CarTargetDamage)
+            dmg:SetDamage(self.TruckTargetDamage)
 
             timer.Simple(1, function()
                 if IsValid(ent) and (ent:IsFrozen() or ent:HasGodMode()) then
@@ -115,10 +114,10 @@ function ENT:StartTouch(ent)
 
             self.ToRemove = true
         else
-            dmg:SetDamage(self.CarDamage)
+            dmg:SetDamage(self.TruckDamage)
         end
 
         ent:TakeDamageInfo(dmg)
-        self:EmitSound("ttt_car_gun/beepbeep.mp3")
+        self:EmitSound("ttt_truck_gun/beepbeep.mp3")
     end
 end
--- gamemodes/terrortown/entities/weapons/weapon_ttt_car_gun_pap.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_car_gun_pap.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile()
 
-local ammoCvar = CreateConVar("ttt_car_gun_ammo", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much ammo the car gun has", 1)
+local ammoCvar = CreateConVar("ttt_pap_car_gun_ammo", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much ammo the truck gun has", 1)
 
 SWEP.PrintName = "Truck Gun"
 SWEP.Spawnable = true
@@ -8,7 +8,7 @@ SWEP.Base = "weapon_tttbase"
 SWEP.Slot = 6
 SWEP.Kind = WEAPON_EQUIP1
 SWEP.InLoadoutFor = nil
-SWEP.LimitedStock = false
+SWEP.LimitedStock = true
 SWEP.AllowDrop = true
 SWEP.IsSilent = false
 SWEP.NoSights = false
@@ -31,16 +31,16 @@ SWEP.DrawCrosshair = true
 SWEP.ViewModel = "models/weapons/c_pistol.mdl"
 SWEP.ViewModelFlip = false
 SWEP.WorldModel = "models/weapons/w_pistol.mdl"
-SWEP.Sound = Sound("ttt_car_gun/beepbeep.mp3")
+SWEP.Sound = Sound("ttt_truck_gun/beepbeep.mp3")
 
 SWEP.CanBuy = {ROLE_TRAITOR}
 
 if CLIENT then
-    SWEP.Icon = "vgui/ttt/weapon_ttt_car_gun.png"
+    SWEP.Icon = "vgui/ttt/weapon_ttt_truck_gun.png"
 
     SWEP.EquipMenuData = {
         type = "Weapon",
-        desc = "Shoot a flying car at someone and freeze them in place!\n\nAnyone caught in the way of the car between you and the victim also takes damage."
+        desc = "Shoot a flying truck at someone and freeze them in place!\n\nAnyone caught in the way of the truck between you and the victim also takes damage."
     }
 end
 
@@ -70,23 +70,44 @@ function SWEP:PrimaryAttack()
     bullet.Damage = 1
     bullet.AmmoType = self.Primary.Ammo
     bullet.TracerName = "PhyscannonImpact"
-    local truck = ents.Create("prop_physics")
-    truck:SetModel("models/ttt_pack_a_punch/semitruck/semitruck.mdl")
-    truck:SetPos(owner:GetPos())
-    -- truck:SetMaterial(TTT_PAP_CAMO)
-    truck:SetMaterial("models/ttt_pack_a_punch/semitruck/semitruck")
-    truck:Spawn()
 
     bullet.Callback = function(att, tr)
-        if SERVER then end -- local victim = tr.Entity -- if IsValid(victim) then -- victim:EmitSound(self.Sound) -- if SERVER and victim:IsPlayer() then -- local victimAim = owner:GetAimVector() -- victimAim.x = -victimAim.x -- victimAim.y = -victimAim.y -- victimAim.z = -victimAim.z -- victimAim = victimAim:Angle() -- victim:SetEyeAngles(victimAim) -- timer.Simple(0, function() -- victim:Lock() -- car:SetPos(owner:EyePos() + owner:GetAimVector() * 100) -- car:SetAngles(owner:EyeAngles()) -- car:SetOwner(owner) -- car.SWEP = self -- car.Target = victim -- end) -- end -- end -- end
-        owner:FireBullets(bullet)
+        if SERVER or (CLIENT and IsFirstTimePredicted()) then
+            local victim = tr.Entity
 
-        if SERVER then
-            self:TakePrimaryAmmo(1)
+            if IsValid(victim) then
+                victim:EmitSound(self.Sound)
 
-            if self:Clip1() <= 0 then
-                self:Remove()
+                if SERVER and victim:IsPlayer() then
+                    local victimAim = owner:GetAimVector()
+                    victimAim.x = -victimAim.x
+                    victimAim.y = -victimAim.y
+                    victimAim.z = -victimAim.z
+                    victimAim = victimAim:Angle()
+                    victim:SetEyeAngles(victimAim)
+
+                    timer.Simple(0, function()
+                        victim:Lock()
+                        local truck = ents.Create("ent_ttt_truck_gun")
+                        truck:SetPos(owner:EyePos() + owner:GetAimVector() * 100)
+                        truck:SetAngles(owner:EyeAngles())
+                        truck:SetOwner(owner)
+                        truck.SWEP = self
+                        truck.Target = victim
+                        truck:Spawn()
+                    end)
+                end
             end
+        end
+    end
+
+    owner:FireBullets(bullet)
+
+    if SERVER then
+        self:TakePrimaryAmmo(1)
+
+        if self:Clip1() <= 0 then
+            self:Remove()
         end
     end
 end
