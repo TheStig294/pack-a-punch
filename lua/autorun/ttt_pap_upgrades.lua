b3790acdeab8cacd83bed513c7904815d019d145
The Stig <TheStig294@users.noreply.github.com>
2023-08-06 16:44:33 +0930

    Moved arsonist igniter upgrade to main file

--- lua/autorun/ttt_pap_upgrades.lua
+++ lua/autorun/ttt_pap_upgrades.lua
@@ -21,6 +21,24 @@ TTT_PAP_UPGRADES = {
             end
         end
     },
+    weapon_ars_igniter = {
+        name = "Buuuuuurn",
+        desc = "Displays the burning elmo meme while held",
+        firerateMult = 1,
+        func = function(SWEP)
+            if SERVER then return end
+            SWEP.ElmoMaterial = Material("ttt_pack_a_punch/arsonist_igniter/elmoburn")
+
+            function SWEP:DrawHUDBackground()
+                surface.SetAlphaMultiplier(0.1)
+                surface.SetDrawColor(39, 39, 39, 39)
+                surface.SetMaterial(self.ElmoMaterial)
+                surface.DrawTexturedRect(0, 0, ScrW(), ScrH())
+                surface.SetAlphaMultiplier(1)
+                if isfunction(self.BaseClass.DrawHUD) then return self.BaseClass.DrawHUD(self) end
+            end
+        end
+    },
     weapon_unoreverse = {
         name = "no u",
         desc = "Lasts twice as long",
