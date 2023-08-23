bd4edf04abb4bd494af01a7227636f83ed4ca400
The Stig <TheStig294@users.noreply.github.com>
2023-08-23 19:39:30 +0930

    Changed cupid's bow upgrade to affect 3 people

---
+++ gamemodes/terrortown/entities/entities/ttt_cup_arrow_pap.lua
@@ -0,0 +1,135 @@
+AddCSLuaFile()
+ENT.Base = "ttt_cup_arrow"
+ENT.PrintName = "PaP Cupid's Arrow"
+ENT.Type = "anim"
+
+local StickSound = {"cupid/impact_arrow_stick_1.wav", "cupid/impact_arrow_stick_2.wav", "cupid/impact_arrow_stick_3.wav"}
+
+local FleshSound = {"cupid/impact_arrow_flesh_1.wav", "cupid/impact_arrow_flesh_2.wav", "cupid/impact_arrow_flesh_3.wav", "cupid/impact_arrow_flesh_4.wav"}
+
+local CollisionIgnoreClasses = {"trigger_*"}
+
+local function ShouldIgnoreCollision(ent)
+    for _, c in ipairs(CollisionIgnoreClasses) do
+        if string.gmatch(ent:GetClass(), c) then return true end
+    end
+
+    return false
+end
+
+function ENT:Touch(ent)
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    local tr = self:GetTouchTrace()
+    local tr2
+
+    if tr.Hit then
+        self:FireBullets({
+            Damage = 0,
+            Attacker = owner,
+            Inflictor = self.Weapon,
+            Callback = function(attacker, trace, damageinfo)
+                tr2 = trace
+            end,
+            Force = 0,
+            Tracer = 0,
+            Src = tr.StartPos,
+            Dir = tr.Normal,
+            AmmoType = "huntingbow_arrows"
+        })
+    end
+
+    if ent and ent:IsWorld() then
+        sound.Play(table.Random(StickSound), tr.HitPos)
+        self:SetMoveType(MOVETYPE_NONE)
+        self:PhysicsInit(SOLID_NONE)
+        SafeRemoveEntityDelayed(self, 10)
+
+        return
+    end
+
+    if not IsValid(ent) then return end
+
+    if ent:IsNPC() or ent:IsPlayer() then
+        if tr2.Entity == ent then
+            sound.Play(table.Random(FleshSound), tr.HitPos)
+        end
+
+        if ent:IsPlayer() and ent:IsActive() then
+            if ent == owner then
+                owner:QueueMessage(MSG_PRINTCENTER, "You cannot make yourself fall in love with someone.")
+            else
+                local target1 = owner:GetNWString("TTTCupidTarget1", "")
+                local target2 = owner:GetNWString("TTTCupidTarget2", "")
+
+                if target1 == "" then
+                    ent:SetNWString("TTTCupidShooter", owner:SteamID64())
+                    owner:SetNWString("TTTCupidTarget1", ent:SteamID64())
+                    owner:QueueMessage(MSG_PRINTBOTH, ent:Nick() .. " has been hit with your first arrow.")
+                    ent:QueueMessage(MSG_PRINTBOTH, "You have been hit by cupid's arrow!")
+                elseif target2 == "" then
+                    if ent:SteamID64() == target1 then
+                        owner:QueueMessage(MSG_PRINTCENTER, "You cannot make someone fall in love with themselves.")
+                    else
+                        ent:SetNWString("TTTCupidShooter", owner:SteamID64())
+                        owner:SetNWString("TTTCupidTarget2", ent:SteamID64())
+                        owner:QueueMessage(MSG_PRINTBOTH, ent:Nick() .. " has been hit with your second arrow.")
+                        ent:QueueMessage(MSG_PRINTBOTH, "You have been hit by cupid's arrow!")
+                    end
+                elseif owner:GetNWString("TTTCupidTarget3", "") == "" then
+                    if ent:SteamID64() == target1 or ent:SteamID64() == target2 then
+                        owner:QueueMessage(MSG_PRINTCENTER, "You cannot make someone fall in love with themselves.")
+                    else
+                        local ent2 = player.GetBySteamID64(target1)
+                        local ent3 = player.GetBySteamID64(target2)
+                        ent:SetNWString("TTTCupidShooter", owner:SteamID64())
+                        owner:SetNWString("TTTCupidTarget3", ent3:SteamID64())
+                        ent:QueueMessage(MSG_PRINTBOTH, "You have fallen in love with " .. ent2:Nick() .. " and " .. ent3:Nick() .. "!")
+                        ent2:QueueMessage(MSG_PRINTBOTH, "You have fallen in love with " .. ent2:Nick() .. " and " .. ent3:Nick() .. "!")
+                        owner:QueueMessage(MSG_PRINTBOTH, "You have created a love triangle between " .. ent:Nick() .. ", " .. ent2:Nick() .. " and " .. ent3:Nick() .. "!")
+                        ent:SetNWString("TTTCupidLover", target1)
+                        ent2:SetNWString("TTTCupidLover", ent:SteamID64())
+                        ent3:SetNWString("TTTCupidLover", ent2:SteamID64())
+                        owner:StripWeapon("weapon_cup_bow")
+                        net.Start("TTT_CupidPaired")
+                        net.WriteString(owner:Nick())
+                        net.WriteString(ent:Nick())
+                        net.WriteString(ent2:Nick())
+                        net.WriteString(owner:SteamID64())
+                        net.Broadcast()
+
+                        -- You need to place successive net messages in a timer, else Gmod will complain about restarting a net message already sent...
+                        timer.Simple(0, function()
+                            net.Start("TTT_CupidPaired")
+                            net.WriteString(owner:Nick())
+                            net.WriteString(ent2:Nick())
+                            net.WriteString(ent3:Nick())
+                            net.WriteString(owner:SteamID64())
+                            net.Broadcast()
+                        end)
+
+                        local mode = GetConVar("ttt_cupid_lovers_notify_mode"):GetInt()
+
+                        if mode ~= ANNOUNCE_REVEAL_NONE then
+                            for _, v in pairs(player.GetAll()) do
+                                if v == ent or v == ent2 or v == owner then continue end
+
+                                if mode == ANNOUNCE_REVEAL_ALL or (v:IsTraitorTeam() and mode == ANNOUNCE_REVEAL_TRAITORS) or (v:IsInnocentTeam() and mode == ANNOUNCE_REVEAL_INNOCENTS) then
+                                    v:QueueMessage(MSG_PRINTBOTH, ROLE_STRINGS_EXT[ROLE_CUPID] .. " has made three players fall in love!")
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        self:Remove()
+    elseif not ShouldIgnoreCollision(ent) then
+        self:SetParent(ent)
+        sound.Play(table.Random(StickSound), tr.HitPos)
+        self:SetMoveType(MOVETYPE_NONE)
+        self:SetSolid(SOLID_NONE)
+        SafeRemoveEntityDelayed(self, 10)
+    end
+end
--- gamemodes/terrortown/entities/weapons/weapon_cup_bow_pap.lua
+++ gamemodes/terrortown/entities/weapons/weapon_cup_bow_pap.lua
@@ -1,87 +1,66 @@
 SWEP.Base = "weapon_cup_bow"
-SWEP.PrintName = "Love Prevails"
-SWEP.PAPDesc = "Lovers aren't soul-linked, you win if even 1 lover survives!"
+SWEP.PrintName = "Love Triangle"
+SWEP.PAPDesc = "You can make 3 people fall in love!"
 
 if SERVER then
-    util.AddNetworkString("TTTPAPCupidBow")
-end
-
-function SWEP:Initialize()
-    if CLIENT then return end
-
-    hook.Add("TTTCupidShouldLoverSurvive", "TTTPAPCupidBow", function(ply, lover)
-        local cupid = player.GetBySteamID64(ply:GetNWString("TTTCupidShooter"))
-        if not IsValid(cupid) then return end
-        local PAPBow = cupid:GetWeapon("weapon_cup_bow_pap")
-
-        -- If the lover's cupid has a Pack-a-Punched bow, let them live!
-        if not IsValid(PAPBow) then
-            return
-        else
-            return true
-        end
-    end)
-
-    hook.Add("TTTCheckForWin", "TTTPAPCupidWinOverride", function()
-        local cupidWin = true
-        local loverAlive = false
-
-        for _, v in pairs(player.GetAll()) do
-            if not v:Alive() or v:IsSpec() then continue end
-            local lover = v:GetNWString("TTTCupidLover", "")
-
-            if lover ~= "" then
-                -- This is the part that checks if a lover has died, we want to skip that!
-                -- 
-                -- local loverPly = player.GetBySteamID64(lover)
-                -- if not IsPlayer(loverPly) or not loverPly:IsActive() then
-                --     cupidWin = false
-                --     break
-                -- end
-                loverAlive = true
-            elseif not v:IsCupid() and not v:ShouldActLikeJester() and not ROLE_HAS_PASSIVE_WIN[v:GetRole()] then
-                cupidWin = false
-                break
+    function SWEP:Initialize()
+        hook.Add("TTTPrepareRound", "TTTPAPResetCupidBow", function()
+            for _, ply in ipairs(player.GetAll()) do
+                ply:SetNWString("TTTCupidTarget3", "")
             end
-        end
 
-        if cupidWin and loverAlive then return WIN_CUPID end
-    end)
-
-    net.Start("TTTPAPCupidBow")
-    net.Broadcast()
-
-    hook.Add("TTTPrepareRound", "TTTPAPCupidBowReset", function()
-        hook.Remove("TTTCupidShouldLoverSurvive", "TTTPAPCupidBow")
-        hook.Remove("TTTCheckForWin", "TTTPAPCupidWinOverride")
-        hook.Remove("TTTPrepareRound", "TTTPAPCupidBowReset")
-    end)
-end
-
-if CLIENT then
-    net.Receive("TTTPAPCupidBow", function()
-        hook.Add("TTTScoringSecondaryWins", "TTTPAPCupidBowWin", function(wintype, secondaryWins)
-            if wintype == WIN_CUPID then return end
-
-            for _, p in ipairs(player.GetAll()) do
-                local lover = p:GetNWString("TTTCupidLover", "")
-
-                if p:Alive() and not p:IsSpec() and lover ~= "" then
-                    local loverPly = player.GetBySteamID64(lover)
-
-                    -- Flipping the check for if a lover has died so we only add the secondary "and the Lovers win" if one lover HAS died...
-                    -- (Else 2 "and the Lovers win" rows are shown...)
-                    if not (IsPlayer(loverPly) and loverPly:Alive() and not loverPly:IsSpec()) then
-                        table.insert(secondaryWins, {
-                            rol = ROLE_CUPID,
-                            txt = LANG.GetTranslation("hilite_lovers_secondary"),
-                            col = ROLE_COLORS[ROLE_CUPID]
-                        })
-                    end
-
-                    return
-                end
-            end
+            hook.Remove("TTTPrepareRound", "TTTPAPResetCupidBow")
         end)
-    end)
+    end
+end
+
+function SWEP:Think()
+    local holdType = self.HoldTypeTranslate[self.dt.WepState]
+
+    if holdType ~= self:GetHoldType() then
+        self:SetHoldType(holdType)
+    end
+
+    if self:GetNextPrimaryFire() >= CurTime() then return end
+    local owner = self:GetOwner()
+
+    if self.dt.WepState == self.STATE_PULLED then
+        if owner:KeyDown(IN_RELOAD) then
+            self.dt.WepState = self.STATE_NOCKED
+            self:RunActivity(ACT_VM_RELEASE)
+        elseif not owner:KeyDown(IN_ATTACK) then
+            self.dt.WepState = self.STATE_RELEASE
+            self:RunActivity(ACT_VM_PRIMARYATTACK)
+
+            if SERVER then
+                local ang = owner:GetAimVector():Angle()
+                local pos = owner:EyePos() + ang:Up() * -7 + ang:Forward() * -4
+
+                if not owner:KeyDown(IN_ATTACK2) then
+                    pos = pos + ang:Right() * 1.5
+                end
+
+                local charge = self:GetNextSecondaryFire()
+                charge = math.Clamp(CurTime() - charge, 0, 1)
+                local arrow = ents.Create("ttt_cup_arrow_pap")
+                arrow:SetOwner(owner)
+                arrow:SetPos(pos)
+                arrow:SetAngles(ang)
+                arrow:Spawn()
+                arrow:Activate()
+                arrow:SetVelocity(ang:Forward() * 2500 * charge)
+                arrow:SetMaterial(TTT_PAP_CAMO)
+                arrow.Weapon = self
+            end
+        end
+    elseif self.dt.WepState == self.STATE_RELEASE then
+        self.dt.WepState = self.STATE_NOCKED
+        self:RunActivity(ACT_VM_LOWERED_TO_IDLE)
+    elseif self.dt.WepState == self.STATE_NOCKED then
+        if owner:KeyDown(IN_ATTACK) and not owner:KeyDown(IN_RELOAD) then
+            self.dt.WepState = self.STATE_PULLED
+            self:RunActivity(ACT_VM_PULLBACK)
+            self:SetNextSecondaryFire(CurTime())
+        end
+    end
 end
