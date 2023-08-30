301b47c7bbbf65c610884caca44dcff1dac66160
The Stig <TheStig294@users.noreply.github.com>
2023-08-30 22:19:21 +0930

    Added upgrade for the randomat club

---
+++ lua/pack_a_punch/upgrades/one_shot_club.lua
@@ -0,0 +1,7 @@
+local UPGRADE = {}
+UPGRADE.id = "one_shot_club"
+UPGRADE.class = "weapon_ttt_randomatclub"
+UPGRADE.name = "One-shot Club"
+UPGRADE.desc = "1-shot kills!"
+UPGRADE.damageMult = 10000
+TTTPAP:Register(UPGRADE)
