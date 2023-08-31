808178d756ff75a8f253f494dff9983e8bcac6d8
The Stig <TheStig294@users.noreply.github.com>
2023-08-31 23:31:49 +0930

    Moved all upgrades without an Apply() function to the generic upgrades folder

--- lua/pack_a_punch/generic_upgrades/murder_revolver.lua
+++ lua/pack_a_punch/generic_upgrades/murder_revolver.lua
@@ -3,9 +3,5 @@ UPGRADE.id = "murder_revolver"
 UPGRADE.class = "weapon_ttt_randomatrevolver"
 UPGRADE.name = "Murder Revolver"
 UPGRADE.desc = "Allows you to reload-cancel"
-
-function UPGRADE:Apply(SWEP)
-    SWEP.Primary.Delay = 2.25
-end
-
+UPGRADE.firerateMult = 2
 TTTPAP:Register(UPGRADE)
