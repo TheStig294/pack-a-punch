bd12026ec9405783ad3eeede5d0af25e161c97f2
The Stig <TheStig294@users.noreply.github.com>
2023-09-01 01:39:15 +0930

    Moved nice pistol to generic upgrades

--- lua/pack_a_punch/generic_upgrades/nice_pistol.lua
+++ lua/pack_a_punch/generic_upgrades/nice_pistol.lua
@@ -3,13 +3,5 @@ UPGRADE.id = "nice_pistol"
 UPGRADE.class = "weapon_ttt_pistol_randomat"
 UPGRADE.name = "Nice Pistol"
 UPGRADE.desc = "Nice ammo count..."
-
-function UPGRADE:Apply(SWEP)
-    SWEP.Primary.ClipSize = 69
-
-    timer.Simple(0.1, function()
-        SWEP:SetClip1(69)
-    end)
-end
-
+UPGRADE.ammoMult = 11.5
 TTTPAP:Register(UPGRADE)
