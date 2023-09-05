3232511f470cacec8bb5c336152d889c776cce6a
The Stig <TheStig294@users.noreply.github.com>
2023-09-05 21:13:32 +0930

    Added spectating the pokeball while captured

--- gamemodes/terrortown/entities/entities/ttt_pap_pokeball.lua
+++ gamemodes/terrortown/entities/entities/ttt_pap_pokeball.lua
@@ -46,30 +46,36 @@ if SERVER then
    end
 
    function ENT:StartTouch(ply)
+      -- Don't try to catch a player if one is already caught
       if IsValid(self.CaughtPly) then return end
       if not IsValid(ply) or not ply:IsPlayer() then return end
       local owner = self.Thrower
-      if not IsValid(owner) or ply == owner then return end
+      -- -- Prevent the player from catching themselves lol
+      -- if not IsValid(owner) or ply == owner then return end
       -- Try to catch the player!
       self:ResetSequence("open")
       self:EmitSound("ttt_pack_a_punch/pokeball/catch.mp3")
       local physObj = self:GetPhysicsObject()
       if not IsValid(physObj) then return end
+      -- Play the capture animation
       physObj:EnableMotion(false)
       ply:Freeze(true)
       ply:SetMaterial("lights/white")
-      ply:SetColor(COLOR_WHITE)
       ply:SetModelScale(0.1, 1)
+      self.CaughtPly = ply
 
+      -- Put the player in the pokeball!
       timer.Simple(1, function()
          if not IsValid(physObj) then return end
          physObj:EnableMotion(true)
          self:PhysWake()
-         self.CaughtPly = ply
-         ply:SetMaterial("")
-         ply:SetModelScale(1, 1)
+         ply:Spectate(OBS_MODE_CHASE)
+         ply:SpectateEntity(self)
+         ply:DrawViewModel(false)
+         ply:DrawWorldModel(false)
       end)
 
+      -- Play the shake sound effect after the ball falls to the ground
       timer.Simple(2, function()
          if not IsValid(self) then return end
          self:EmitSound("ttt_pack_a_punch/pokeball/shake.mp3")
@@ -82,9 +88,26 @@ if SERVER then
          if not IsValid(self) then return end
          local captureChancePercent = ply:GetMaxHealth() - math.min(ply:Health(), ply:GetMaxHealth()) + self.MinCatchChance
 
-         -- Capture failed!
          if math.random() > captureChancePercent / 100 then
+            -- Capture failed!
             self:EmitSound("ttt_pack_a_punch/pokeball/release.mp3")
+            ply:UnSpectate()
+            ply:Spawn()
+            ply:SetPos(self:GetPos())
+            ply:DrawViewModel(true)
+            ply:DrawWorldModel(true)
+            ply:SetModelScale(1, 1)
+            ply:SetMaterial("lights/white")
+            ply:Freeze(false)
+
+            timer.Simple(1, function()
+               if not IsValid(ply) then return end
+               ply:SetMaterial("")
+            end)
+
+            self:Remove()
+
+            return
          else
             -- Capture success!
             self:EmitSound("ttt_pack_a_punch/pokeball/capture.mp3")
