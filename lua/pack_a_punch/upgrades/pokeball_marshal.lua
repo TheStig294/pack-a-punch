2f2cb413a71599a0d98ee2753d5a8ccc8f1f01e9
The Stig <TheStig294@users.noreply.github.com>
2023-09-05 14:17:22 +0930

    Removed camo from pokeball upgrade

--- lua/pack_a_punch/upgrades/pokeball_marshal.lua
+++ lua/pack_a_punch/upgrades/pokeball_marshal.lua
@@ -4,4 +4,5 @@ UPGRADE.class = "weapon_mhl_badge"
 UPGRADE.name = "Sheriff Badge"
 UPGRADE.desc = "Promote players by catching them in a Pokeball!"
 UPGRADE.newClass = "ttt_pap_pokeball_swep"
+UPGRADE.noCamo = true
 TTTPAP:Register(UPGRADE)
