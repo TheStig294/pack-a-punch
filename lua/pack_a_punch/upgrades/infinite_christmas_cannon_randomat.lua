2e12a44a11552a8cf6429327aa7e0ecba7e42206
The Stig <TheStig294@users.noreply.github.com>
2023-09-11 20:13:51 +0930

    Disabled the coal piece shooting for the randomat christmas cannon upgrade

--- lua/pack_a_punch/upgrades/infinite_christmas_cannon_randomat.lua
+++ lua/pack_a_punch/upgrades/infinite_christmas_cannon_randomat.lua
@@ -2,9 +2,9 @@ local UPGRADE = {}
 UPGRADE.id = "infinite_christmas_cannon_randomat"
 UPGRADE.class = "weapon_randomat_christmas_cannon"
 UPGRADE.name = "Infinite Christmas Cannon"
-UPGRADE.desc = "Infinite PaP presents and coal piece shooting!"
+UPGRADE.desc = "Infinite Pack-a-Punch presents!"
 
--- All credit goes to Mal and Nick for creating the Santa role and randomat, including the below functions!
+-- All credit goes to Mal and Nick for creating the Santa role and randomat, including the below function!
 function UPGRADE:Apply(SWEP)
     function SWEP:PrimaryAttack()
         if not IsFirstTimePredicted() then return end
@@ -37,32 +37,8 @@ function UPGRADE:Apply(SWEP)
         owner:ViewPunch(Angle(util.SharedRandom(self:GetClass(), -0.2, -0.1, 0) * self.Primary.Recoil, util.SharedRandom(self:GetClass(), -0.1, 0.1, 1) * self.Primary.Recoil, 0))
     end
 
+    -- Disable coal-piece shooting
     function SWEP:SecondaryAttack()
-        if not IsFirstTimePredicted() then return end
-        self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-        self:SetNextSecondaryFire(CurTime() + self.Primary.Delay)
-        local owner = self:GetOwner()
-        if not IsValid(owner) then return end
-        self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-        self:EmitSound("weapons/grenade_launcher1.wav")
-
-        if SERVER then
-            local coal = ents.Create("ttt_santa_coal")
-            if not coal:IsValid() then return false end
-            local ang = owner:EyeAngles()
-            coal:SetAngles(ang)
-            coal:SetPos(owner:GetShootPos() + ang:Forward() * 50 + ang:Right() * 1 - ang:Up() * 1)
-            coal:SetOwner(owner)
-            coal:Spawn()
-            coal:SetColor(Color(128, 128, 128, 255))
-            local physobj = coal:GetPhysicsObject()
-
-            if IsValid(physobj) then
-                physobj:SetVelocity(owner:GetAimVector() * 1500)
-            end
-        end
-
-        owner:ViewPunch(Angle(util.SharedRandom(self:GetClass(), -0.2, -0.1, 0) * self.Primary.Recoil, util.SharedRandom(self:GetClass(), -0.1, 0.1, 1) * self.Primary.Recoil, 0))
     end
 end
 
