17c49386c9df4190e3534df77acadf767208f1ae
The Stig <TheStig294@users.noreply.github.com>
2023-09-12 15:49:43 +0930

    Added upgrade for the Frenchman's baguette

---
+++ lua/pack_a_punch/upgrades/shouting_baguette.lua
@@ -0,0 +1,17 @@
+local UPGRADE = {}
+UPGRADE.id = "shouting_baguette"
+UPGRADE.class = "weapon_fre_baguette"
+UPGRADE.name = "Baguette Qui Crie"
+UPGRADE.desc = "Joue des cris en tuant quelqu'un"
+
+function UPGRADE:Apply()
+    self:AddHook("DoPlayerDeath", function(_, attacker, _)
+        if not IsValid(attacker) or not attacker:IsPlayer() then return end
+
+        if attacker:HasWeapon(self.class) and attacker:GetWeapon(self.class).PAPUpgrade then
+            attacker:EmitSound("frenchman/death" .. math.random(1, 6) .. ".mp3")
+        end
+    end)
+end
+
+TTTPAP:Register(UPGRADE)
