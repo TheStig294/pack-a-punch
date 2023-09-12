63f1feda4fc81cea5bafe247af713864ede61a1a
The Stig <TheStig294@users.noreply.github.com>
2023-09-12 15:35:15 +0930

    Added upgrade for the possum disguiser

---
+++ lua/pack_a_punch/upgrades/possum_disguiser.lua
@@ -0,0 +1,40 @@
+local UPGRADE = {}
+UPGRADE.id = "possum_disguiser"
+UPGRADE.class = "weapon_psm_disguiser"
+UPGRADE.name = "Possum Disguise"
+UPGRADE.desc = "Increased disguiser capacity"
+local possumModel = "models/TSBB/Animals/Possum.mdl"
+local possumInstalled = util.IsValidModel(possumModel)
+
+if possumInstalled then
+    UPGRADE.desc = UPGRADE.desc .. "\nBecome a possum while disguised!"
+end
+
+UPGRADE.convars = {
+    {
+        name = "pap_possum_disguiser_ammo_mult",
+        type = "float",
+        decimals = 1
+    }
+}
+
+local ammoMultCvar = CreateConVar("pap_possum_disguiser_ammo_mult", "2", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Disguiser capacity multiplier", 1, 5)
+
+function UPGRADE:Apply(SWEP)
+    -- Ammo increase
+    local ammo = ammoMultCvar:GetFloat() * 100
+    SWEP.Primary.ClipSize = ammo
+    SWEP.Primary.ClipMax = ammo
+    SWEP.Primary.DefaultClip = ammo
+
+    if possumInstalled then
+        self:AddHook("PlayerPostThink", function(ply)
+            -- Set model on disguise
+            if IsValid(ply.possumRagdoll) and ply:HasWeapon(self.class) and ply:GetWeapon(self.class).PAPUpgrade then
+                ply.possumRagdoll:SetModel(possumModel)
+            end
+        end)
+    end
+end
+
+TTTPAP:Register(UPGRADE)
