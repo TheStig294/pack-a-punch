6fb96d37eca48a14df8195d6521c1831cf38353b
The Stig <TheStig294@users.noreply.github.com>
2023-09-14 19:23:07 +0930

    Fixed convar descriptions for all stats up generic upgrade

--- lua/pack_a_punch/generic_upgrades/all_stats_up.lua
+++ lua/pack_a_punch/generic_upgrades/all_stats_up.lua
@@ -6,13 +6,13 @@ UPGRADE.desc = "All stats up!"
 
 local firerateCvar = CreateConVar("pap_all_stats_up_firerate", "1.1", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 1, 10)
 
-local damageCvar = CreateConVar("pap_all_stats_up_damage", "1.1", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 1, 10)
+local damageCvar = CreateConVar("pap_all_stats_up_damage", "1.1", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Damage multiplier", 1, 10)
 
-local spreadCvar = CreateConVar("pap_all_stats_up_spread", "1.3", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 1, 10)
+local spreadCvar = CreateConVar("pap_all_stats_up_spread", "1.3", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Spread multiplier", 1, 10)
 
-local ammoCvar = CreateConVar("pap_all_stats_up_ammo", "1.2", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 1, 10)
+local ammoCvar = CreateConVar("pap_all_stats_up_ammo", "1.2", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Ammo multiplier", 1, 10)
 
-local recoilCvar = CreateConVar("pap_all_stats_up_recoil", "0.75", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 0, 1)
+local recoilCvar = CreateConVar("pap_all_stats_up_recoil", "0.75", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Recoil multiplier", 0, 1)
 
 UPGRADE.convars = {
     {
