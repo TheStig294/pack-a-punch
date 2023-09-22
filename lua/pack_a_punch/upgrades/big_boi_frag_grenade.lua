d245ae66402418ff2d89cf559d1913773fcdcb6c
The Stig <TheStig294@users.noreply.github.com>
2023-09-22 18:53:14 +0930

    Added upgrade to frag grenade

---
+++ lua/pack_a_punch/upgrades/big_boi_frag_grenade.lua
@@ -0,0 +1,26 @@
+local UPGRADE = {}
+UPGRADE.id = "big_boi_frag_grenade"
+UPGRADE.class = "weapon_ttt_frag"
+UPGRADE.name = "Big Boi Frag Grenade"
+
+local radiusCvar = CreateConVar("pap_big_boi_frag_grenade_radius", "2", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Explosion radius", 0, 10)
+
+UPGRADE.desc = "x" .. radiusCvar:GetFloat() .. " explosion radius!"
+
+UPGRADE.convars = {
+    {
+        name = "pap_big_boi_frag_grenade_radius",
+        type = "float",
+        decimals = 1
+    }
+}
+
+function UPGRADE:Apply(SWEP)
+    if SERVER then
+        function SWEP:GetGrenadeName()
+            return "ttt_pap_frag_proj"
+        end
+    end
+end
+
+TTTPAP:Register(UPGRADE)
