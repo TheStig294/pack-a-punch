a6f969373866f727da9273023d5fe35959ec11d7
The Stig <TheStig294@users.noreply.github.com>
2023-09-22 18:30:57 +0930

    Added upgrade for the silent fox

---
+++ lua/pack_a_punch/generic_upgrades/loud_fox.lua
@@ -0,0 +1,18 @@
+local UPGRADE = {}
+UPGRADE.id = "loud_fox"
+UPGRADE.class = "weapon_ttt_tmp_s"
+UPGRADE.name = "Loud Fox"
+
+local multCvar = CreateConVar("pap_loud_fox_firerate", "1.5", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Firerate multiplier", 1, 5)
+
+UPGRADE.desc = "x" .. multCvar:GetFloat() .. " firerate, unsilenced"
+
+UPGRADE.convars = {
+    {
+        name = "pap_loud_fox_firerate",
+        type = "int"
+    }
+}
+
+UPGRADE.firerateMult = multCvar:GetFloat()
+TTTPAP:Register(UPGRADE)
