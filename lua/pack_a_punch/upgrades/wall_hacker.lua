90e6249b199ed396711f389de27aca985f9c2083
The Stig <thestig294@gmail.com>
2023-09-25 14:35:15 +0930

    Added upgrade for the player pinger

---
+++ lua/pack_a_punch/upgrades/wall_hacker.lua
@@ -0,0 +1,57 @@
+local UPGRADE = {}
+UPGRADE.id = "wall_hacker"
+UPGRADE.class = "ttt_player_pinger"
+UPGRADE.name = "Wall Hacker"
+UPGRADE.desc = "Lets everyone see the outlines!\nIf you're a baddie, just your teammates!"
+
+function UPGRADE:Apply(SWEP)
+    SWEP.PAPOldPrimaryAttack = SWEP.PrimaryAttack
+
+    function SWEP:PrimaryAttack()
+        self:PAPOldPrimaryAttack()
+        if CLIENT then return end
+        local owner = self:GetOwner()
+        if not UPGRADE:IsPlayer(owner) then return end
+
+        if owner.IsTraitorTeam and owner:IsTraitorTeam() then
+            -- If a traitor uses an upgraded player pinger, and CR is installed, all traitor team members get wall hacks
+            for _, ply in ipairs(UPGRADE:GetAlivePlayers()) do
+                if ply == owner then continue end
+
+                if ply.IsTraitorTeam and ply:IsTraitorTeam() then
+                    ply:PrintMessage(HUD_PRINTTALK, "A fellow traitor used an upgraded player pinger, you recieved temorary wall hacks!")
+                    self:ActivatePing(ply)
+                end
+            end
+        elseif owner:GetRole() == ROLE_TRAITOR then
+            -- If a traitor uses an upgraded player pinger, and CR isn't installed, all traitors get wall hacks
+            for _, ply in ipairs(UPGRADE:GetAlivePlayers()) do
+                if ply == owner then continue end
+
+                if ply:GetRole() == ROLE_TRAITOR then
+                    ply:PrintMessage(HUD_PRINTTALK, "A fellow traitor used an upgraded player pinger, you recieved temorary wall hacks!")
+                    self:ActivatePing(ply)
+                end
+            end
+        elseif owner.IsIndependentTeam and owner:IsIndependentTeam() then
+            -- If an independent role uses an upgraded player pinger, all players with the same role as the owner get wall hacks
+            for _, ply in ipairs(UPGRADE:GetAlivePlayers()) do
+                if ply == owner then continue end
+
+                if ply:GetRole() == owner:GetRole() then
+                    ply:PrintMessage(HUD_PRINTTALK, "A teammate used an upgraded player pinger, you recieved temorary wall hacks!")
+                    self:ActivatePing(ply)
+                end
+            end
+        else
+            -- Otherwise, all players get wall hacks
+            for _, ply in ipairs(UPGRADE:GetAlivePlayers()) do
+                if ply == owner then continue end
+                ply:PrintMessage(HUD_PRINTTALK, "Someone used an upgraded player pinger, you recieved temorary wall hacks!")
+                self:ActivatePing(ply)
+            end
+        end
+    end
+end
+
+TTTPAP:Register(UPGRADE)
