ff5dd4b5c4dc80dc3aaefd5a61f927f2c5914dac
The Stig <thestig294@gmail.com>
2023-09-27 22:52:57 +0930

    Added upgrade for the laser pointer

---
+++ lua/pack_a_punch/generic_upgrades/infinite_laser.lua
@@ -0,0 +1,7 @@
+local UPGRADE = {}
+UPGRADE.id = "infinite_laser"
+UPGRADE.class = "laserpointer"
+UPGRADE.name = "Infinite Laser"
+UPGRADE.desc = "x999 ammo lol"
+UPGRADE.ammoMult = 999
+TTTPAP:Register(UPGRADE)
