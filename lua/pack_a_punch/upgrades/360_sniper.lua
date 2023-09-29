67218d5654915cf084eac466058fe633a78502d0
The Stig <thestig294@gmail.com>
2023-09-29 22:34:23 +0930

    Added an upgrade for the combine sniper

---
+++ lua/pack_a_punch/upgrades/360_sniper.lua
@@ -0,0 +1,63 @@
+local UPGRADE = {}
+UPGRADE.id = "360_sniper"
+UPGRADE.class = "ttt_combine_sniper_summoner"
+UPGRADE.name = "360 Sniper"
+UPGRADE.desc = "Spins around on the spot, look out!"
+
+function UPGRADE:Apply(SWEP)
+    function SWEP:place_sniper(tracedata)
+        if CLIENT then return end
+        local ent = ents.Create("npc_sniper")
+
+        for k, v in pairs(player.GetAll()) do
+            v:ChatPrint("Look out for an upgraded combine sniper! They like to 360...")
+        end
+
+        if not IsValid(ent) then return end
+        local spawnereasd = self:FindRespawnLocationCustom(tracedata.pos)
+
+        if spawnereasd ~= false then
+            local pitch, yaw, roll = self:GetOwner():EyeAngles():Unpack()
+            pitch = 0
+            ent:SetPos(spawnereasd)
+            ent:SetAngles(Angle(pitch, yaw, roll))
+            ent:Spawn()
+            ent:SetMaterial(TTTPAP.camo)
+
+            timer.Create("CombineSniperRotate" .. ent:EntIndex(), 0.1, 0, function()
+                if IsValid(ent) then
+                    local angles = ent:GetAngles()
+                    angles:Add(Angle(0, 5, 0))
+                    ent:SetAngles(angles)
+                else
+                    -- Remove the timer as soon as the entity is no longer valid,
+                    -- Such as at the end of the round when TTT removes all non-map entities
+                    timer.Remove("CombineSniperRotate" .. ent:EntIndex())
+                end
+            end)
+
+            if ConVarExists("ttt_combine_sniper_remove") and GetConVar("ttt_combine_sniper_remove"):GetBool() then
+                -- If for whatever reason the remove time cannot be read, set the remove timer to 15 seconds
+                local removeTime = GetConVar("ttt_combine_sniper_time"):GetInt() or 15
+
+                timer.Create("CombineSniperRemove" .. ent:EntIndex(), removeTime, 1, function()
+                    timer.Remove("CombineSniperRotate" .. ent:EntIndex())
+
+                    if IsValid(ent) then
+                        ent:Remove()
+                    end
+                end)
+            end
+        end
+
+        local phys = ent:GetPhysicsObject()
+
+        if not IsValid(phys) then
+            ent:Remove()
+
+            return
+        end
+    end
+end
+
+TTTPAP:Register(UPGRADE)
