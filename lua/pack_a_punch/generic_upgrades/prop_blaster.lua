590595906f1e017b3a0c236ce6006c0ac942e1b8
The Stig <thestig294@gmail.com>
2023-09-30 00:53:24 +0930

    Added an upgrade for the clutterbomb

---
+++ lua/pack_a_punch/generic_upgrades/prop_blaster.lua
@@ -0,0 +1,7 @@
+local UPGRADE = {}
+UPGRADE.id = "prop_blaster"
+UPGRADE.class = "weapon_ttt_clutterbomb"
+UPGRADE.name = "Prop Blaster"
+UPGRADE.desc = "Now a full-blown prop blaster!"
+UPGRADE.newClass = "weapon_prop_blaster"
+TTTPAP:Register(UPGRADE)
