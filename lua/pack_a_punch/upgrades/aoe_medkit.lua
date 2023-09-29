519bedc0305b94bf3959cd73aed30dd7bc9a822c
The Stig <thestig294@gmail.com>
2023-09-29 16:39:19 +0930

    Added upgrade to the medkit

--- lua/pack_a_punch/upgrades/aoe_medkit.lua
+++ lua/pack_a_punch/upgrades/aoe_medkit.lua
@@ -2,38 +2,54 @@ local UPGRADE = {}
 UPGRADE.id = "aoe_medkit"
 UPGRADE.class = "weapon_ttt_medkit"
 UPGRADE.name = "AOE Medkit"
-UPGRADE.desc = "Heals you more, and other players around you!"
+UPGRADE.desc = "Heals you more at once, and other players around you!"
+
+UPGRADE.convars = {
+    {
+        name = "pap_aoe_medkit_heal_mult",
+        type = "float",
+        decimals = 1
+    }
+}
+
+local healMultCvar = CreateConVar("pap_aoe_medkit_heal_mult", 1.5, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Extra healing mutiplier", 0.1, 10)
+
+UPGRADE.ammoMult = healMultCvar:GetFloat()
 
 function UPGRADE:Apply(SWEP)
-    SWEP.HealAmount = SWEP.HealAmount * 2
+    SWEP.HealAmount = SWEP.HealAmount * healMultCvar:GetFloat()
+    SWEP.MaxAmmo = SWEP.MaxAmmo * healMultCvar:GetFloat()
 
     function SWEP:SecondaryAttack()
         if CLIENT then return end
         local owner = self:GetOwner()
-        local need = self.HealAmount
 
-        if IsValid(owner) and self:Clip1() >= need and owner:Health() < owner:GetMaxHealth() then
-            self:TakePrimaryAmmo(need)
+        for _, ply in ipairs(ents.FindInSphere(self:GetPos(), 200)) do
+            if not UPGRADE:IsPlayer(ply) then continue end
+            self.PAPTakeAmmo = true
 
-            for _, ply in ipairs(ents.FindInSphere(self:GetPos(), 200)) do
-                if not UPGRADE:IsPlayer(ply) then continue end
-                need = math.min(ply:GetMaxHealth() - ply:Health(), self.HealAmount)
-                ply:SetHealth(math.min(ply:GetMaxHealth(), ply:Health() + need))
-            end
-
-            owner:EmitSound(HealSound)
-            self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-            self:SetNextSecondaryFire(CurTime() + self:SequenceDuration() + 0.5)
-            owner:SetAnimation(PLAYER_ATTACK1)
-
-            timer.Create("weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function()
-                if IsValid(self) then
-                    self:SendWeaponAnim(ACT_VM_IDLE)
+            if self:Clip1() >= self.HealAmount and ply:Health() < ply:GetMaxHealth() then
+                if self.PAPTakeAmmo then
+                    self:TakePrimaryAmmo(self.HealAmount)
                 end
-            end)
-        else
-            owner:EmitSound(DenySound)
-            self:SetNextSecondaryFire(CurTime() + 1)
+
+                self.PAPTakeAmmo = false
+                self.HealAmount = math.min(ply:GetMaxHealth() - ply:Health(), self.HealAmount)
+                ply:SetHealth(math.min(ply:GetMaxHealth(), ply:Health() + self.HealAmount))
+                ply:EmitSound("items/smallmedkit1.wav")
+                self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+                self:SetNextSecondaryFire(CurTime() + self:SequenceDuration() + 0.5)
+                owner:SetAnimation(PLAYER_ATTACK1)
+
+                timer.Create("weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function()
+                    if IsValid(self) then
+                        self:SendWeaponAnim(ACT_VM_IDLE)
+                    end
+                end)
+            else
+                owner:EmitSound("items/medshotno1.wav")
+                self:SetNextSecondaryFire(CurTime() + 1)
+            end
         end
     end
 end
