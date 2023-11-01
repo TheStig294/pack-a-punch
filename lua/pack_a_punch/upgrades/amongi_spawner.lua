ccfbecbca533916dba67804b39f53608b9e872aa
The Stig <thestig294@gmail.com>
2023-11-01 13:38:41 +1030

    Fixed upgraded amongus summoner's spawn location moving as more amongus are spawned

--- lua/pack_a_punch/upgrades/amongi_spawner.lua
+++ lua/pack_a_punch/upgrades/amongi_spawner.lua
@@ -28,62 +28,28 @@ function UPGRADE:Apply(SWEP)
         end
     end
 
-    local function FindRespawnLocCust(pos)
-        local offsets = {Vector(0, 0, 0)}
+    if SERVER then
+        self:AddHook("EntityRemoved", function(ent)
+            if not ent.PAPAmongiSpawnerOwner then return end
+            local owner = ent.PAPAmongiSpawnerOwner
+            local pos = ent:GetPos()
 
-        for i = 0, 360, 15 do
-            table.insert(offsets, Vector(math.sin(i), math.cos(i), 0))
-        end
-
-        local midsize = Vector(34, 34, 76)
-        local tstart = pos + Vector(0, 0, midsize.z / 2)
-
-        for i = 1, #offsets do
-            local o = offsets[i]
-            local v = tstart + o * midsize * 1.5
-
-            local t = {
-                start = v,
-                endpos = v,
-                --filter = target,
-                mins = midsize / -2,
-                maxs = midsize / 2
-            }
-
-            local tr = util.TraceHull(t)
-            if not tr.Hit then return v - Vector(0, 0, midsize.z / 2) end
-        end
-
-        return false
+            if self:IsPlayer(owner) then
+                timer.Simple(2, function()
+                    -- Don't spawn if the round has restarted
+                    if GetRoundState() == ROUND_PREP then return end
+                    local amongus = ents.Create("amongus_spawner")
+                    if not IsValid(amongus) then return end
+                    amongus.PAPAmongiSpawnerOwner = owner
+                    -- Don't use the amongus spawner's place_amongus() function, as it causes weird "drifting" of the amongus spawn location
+                    -- as more are spawned from the one entity
+                    amongus:SetOwner(owner)
+                    amongus:SetPos(pos)
+                    amongus:Spawn()
+                end)
+            end
+        end)
     end
-
-    local function place_amongus(pos, owner)
-        if CLIENT then return end
-        local amongus = ents.Create("amongus_spawner")
-        if not IsValid(amongus) then return end
-        amongus.PAPAmongiSpawnerOwner = owner
-        local spawnereasd = FindRespawnLocCust(pos)
-
-        if spawnereasd then
-            amongus:SetOwner(owner)
-            amongus:SetPos(spawnereasd)
-            amongus:Spawn()
-        end
-    end
-
-    self:AddHook("EntityRemoved", function(ent)
-        if not ent.PAPAmongiSpawnerOwner then return end
-        local owner = ent.PAPAmongiSpawnerOwner
-        local pos = ent:GetPos()
-
-        if self:IsPlayer(owner) then
-            timer.Simple(2, function()
-                -- Don't spawn if the round has restarted
-                if GetRoundState() == ROUND_PREP then return end
-                place_amongus(pos, owner)
-            end)
-        end
-    end)
 end
 
 TTTPAP:Register(UPGRADE)
