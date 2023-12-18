f3d2c7ba0fe8cbff5eaed48f766bd9c879be861f
The Stig <thestig294@gmail.com>
2023-12-18 15:35:02 +0000

    Workaround fix to slappers weapon being weird

--- lua/pack_a_punch/upgrades/spinning_slappers.lua
+++ lua/pack_a_punch/upgrades/spinning_slappers.lua
@@ -24,17 +24,40 @@ function UPGRADE:Apply(SWEP)
     SWEP.PAPFlingSound = Sound("ttt_pack_a_punch/flinging_spin_attack/fling.mp3")
     SWEP.Primary.Delay = cooldownCvar:GetInt()
 
+    -- Hack for setting .Spinning property because SWEP:PrimaryAttack isn't called on the server after swapping weapons for some reason...
+    if SERVER then
+        util.AddNetworkString("TTTPAPSpinningSlappers")
+
+        net.Receive("TTTPAPSpinningSlappers", function(len, ply)
+            local spinning = net.ReadBool()
+            local slappers = net.ReadEntity()
+
+            if IsValid(slappers) then
+                slappers.Spinning = spinning
+                local owner = slappers:GetOwner()
+
+                if spinning and IsValid(owner) then
+                    owner:EmitSound("ttt_pack_a_punch/spinning_slappers/spinattack.mp3")
+                end
+            end
+        end)
+    end
+
     function SWEP:PrimaryAttack()
+        if SERVER then return end
         self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-
-        if SERVER then
-            self:GetOwner():EmitSound(Sound("ttt_pack_a_punch/spinning_slappers/spinattack.mp3"))
-        end
-
         self.Spinning = true
+        net.Start("TTTPAPSpinningSlappers")
+        net.WriteBool(self.Spinning)
+        net.WriteEntity(self)
+        net.SendToServer()
 
         timer.Simple(0.49, function()
             self.Spinning = false
+            net.Start("TTTPAPSpinningSlappers")
+            net.WriteBool(self.Spinning)
+            net.WriteEntity(self)
+            net.SendToServer()
         end)
     end
 
@@ -43,29 +66,34 @@ function UPGRADE:Apply(SWEP)
     function SWEP:Think()
         SWEP:PAPOldThink()
 
-        if self.Spinning and SERVER then
+        if self.Spinning then
             local owner = self:GetOwner()
-            owner:SetEyeAngles(Angle(0, owner:EyeAngles().y + 20, 0))
 
-            for i, ply in pairs(player.GetAll()) do
-                if (ply ~= owner) and ply:Alive() and not ply:IsSpec() and (owner:GetPos():DistToSqr(ply:GetPos()) < (150 * 150)) and not ply.PAPSpinningSlappersCooldown then
-                    ply.PAPSpinningSlappersCooldown = true
-                    self:Slap()
-                    ply:EmitSound(self.PAPFlingSound)
-                    -- Use vector subtraction to get the direction vector to know which way to fling the player
-                    local ownerPos = owner:GetPos()
-                    local plyPos = ply:GetPos()
-                    local flingDirection = ownerPos - plyPos
-                    flingDirection:Normalize()
-                    flingDirection:Mul(strengthCvar:GetInt())
-                    flingDirection.z = strengthCvar:GetInt()
-                    ply:SetVelocity(flingDirection)
+            if CLIENT then
+                owner:SetEyeAngles(Angle(0, owner:EyeAngles().y + 20, 0))
+            end
 
-                    timer.Simple(cooldownCvar:GetInt(), function()
-                        if IsValid(ply) then
-                            ply.PAPSpinningSlappersCooldown = nil
-                        end
-                    end)
+            if SERVER then
+                for _, ply in pairs(player.GetAll()) do
+                    if (ply ~= owner) and ply:Alive() and not ply:IsSpec() and (owner:GetPos():DistToSqr(ply:GetPos()) < (150 * 150)) and not ply.PAPSpinningSlappersCooldown then
+                        ply.PAPSpinningSlappersCooldown = true
+                        self:Slap()
+                        ply:EmitSound(self.PAPFlingSound)
+                        -- Use vector subtraction to get the direction vector to know which way to fling the player
+                        local ownerPos = owner:GetPos()
+                        local plyPos = ply:GetPos()
+                        local flingDirection = ownerPos - plyPos
+                        flingDirection:Normalize()
+                        flingDirection:Mul(strengthCvar:GetInt())
+                        flingDirection.z = strengthCvar:GetInt()
+                        ply:SetVelocity(flingDirection)
+
+                        timer.Simple(cooldownCvar:GetInt(), function()
+                            if IsValid(ply) then
+                                ply.PAPSpinningSlappersCooldown = nil
+                            end
+                        end)
+                    end
                 end
             end
         end
