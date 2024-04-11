2309bb981c9ec80baaec053418d30057c70e931d
The Stig <thestig294@gmail.com>
2024-04-11 13:53:40 +0930

    - Added clear descriptions to the death note upgrade that display on switching the cause of death - Made the sounds played by the upgraded death note louder - Fixed the upgraded explosion cause of death giving you the death note back - Added a message for the owner and victim of the upgraded death note on the victim dying

--- lua/pack_a_punch/upgrades/dramatic_death_note.lua
+++ lua/pack_a_punch/upgrades/dramatic_death_note.lua
@@ -2,14 +2,114 @@ local UPGRADE = {}
 UPGRADE.id = "dramatic_death_note"
 UPGRADE.class = "death_note_ttt"
 UPGRADE.name = "Dramatic Death Note"
-UPGRADE.desc = "Deaths are more... dramatic?"
+UPGRADE.desc = "Each death type has a new effect!"
+
+local upgradeDescriptions = {
+    prematureburial = "Victim sinks way further into the ground, plays the lego yoda death noise",
+    ignite = "x5 fire damage, starts fires after they die",
+    heartattack = "Plays the vine boom",
+    fall = "Plays a cartoon fling noise",
+    explode = "Extra large explosion and starts some fires!"
+}
 
 function UPGRADE:Apply(SWEP)
-    SWEP:GetOwner().PAPDramaticDeathNote = true
+    local own = SWEP:GetOwner()
+
+    if IsValid(own) then
+        own.PAPDramaticDeathNote = true
+    end
+
+    function SWEP:OwnerChanged()
+        local owner = SWEP:GetOwner()
+
+        if IsValid(owner) then
+            owner.PAPDramaticDeathNote = true
+        end
+    end
+
+    self:AddToHook(SWEP, "PrimaryAttack", function()
+        if IsFirstTimePredicted() and SERVER then
+            SWEP:GetOwner():ChatPrint(upgradeDescriptions[DN_DeathTypes[SWEP.TTTDeathType]])
+        end
+    end)
+
+    self:AddHook("dn_module_prematureburial", function(owner, victim)
+        if not owner.PAPDramaticDeathNote then return end
+        local timername = owner:SteamID64() .. "TTTPAPDramaticDeathNoteBurial"
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/lego_yoda.mp3")
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/lego_yoda.mp3")
+
+        timer.Create(timername, 0.01, 500, function()
+            if GetRoundState() ~= ROUND_PREP and self:IsAlive(victim) then
+                local pos = victim:GetPos()
+                pos.z = pos.z - 1
+                victim:SetPos(pos)
+            else
+                timer.Remove(timername)
+
+                return
+            end
+        end)
+
+        timer.Simple(0, function()
+            owner:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.prematureburial)
+            victim:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.prematureburial)
+        end)
+    end)
+
+    self:AddHook("dn_module_ignite", function(owner, victim)
+        if not owner.PAPDramaticDeathNote then return end
+        victim.PAPDramaticDeathNoteIgnite = true
+
+        timer.Simple(0, function()
+            owner:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.ignite)
+            victim:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.ignite)
+        end)
+    end)
+
+    self:AddHook("EntityTakeDamage", function(ent, dmg)
+        if ent.PAPDramaticDeathNoteIgnite and dmg:IsDamageType(DMG_BURN) then
+            dmg:ScaleDamage(5)
+        end
+    end)
+
+    self:AddHook("PostPlayerDeath", function(ply)
+        if ply.PAPDramaticDeathNoteIgnite then
+            local tr = util.QuickTrace(ply:GetPos(), Vector(0, 0, -1))
+            StartFires(ply:GetPos(), tr, 20, 40, false, ply)
+            ply.PAPDramaticDeathNoteIgnite = nil
+        end
+    end)
+
+    self:AddHook("dn_module_heartattack", function(owner, victim)
+        if not owner.PAPDramaticDeathNote then return end
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/vine_boom.mp3")
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/vine_boom.mp3")
+
+        timer.Simple(0, function()
+            owner:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.heartattack)
+            victim:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.heartattack)
+        end)
+    end)
+
+    self:AddHook("dn_module_fall", function(owner, victim)
+        if not owner.PAPDramaticDeathNote then return end
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/cartoon_fling_sound.mp3")
+        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/cartoon_fling_sound.mp3")
+
+        timer.Simple(0, function()
+            owner:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.fall)
+            victim:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.fall)
+        end)
+    end)
 
     self:AddHook("dn_module_explode", function(owner, victim)
         if not owner.PAPDramaticDeathNote then return end
         local timername = owner:SteamID64() .. "TTTPAPDramaticDeathNoteExplode"
+
+        timer.Simple(1, function()
+            timer.Remove("Expolde_Countdown")
+        end)
 
         timer.Create(timername, 1, GetConVar("DeathNote_ExplodeTimer"):GetInt(), function()
             if GetRoundState() == ROUND_PREP then
@@ -30,59 +130,13 @@ function UPGRADE:Apply(SWEP)
                     explode:Fire("Explode", 0, 0)
                     local tr = util.QuickTrace(victim:GetPos(), Vector(0, 0, -1))
                     StartFires(victim:GetPos(), tr, 20, 40, false, owner)
-                    owner:ChatPrint("DeathNote upgrade: Killed with extra large explosion and started some fires!")
+                    owner:ChatPrint("Death Note: " .. victim:Nick() .. " exploded!")
+
+                    timer.Simple(0, function()
+                        owner:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.explode)
+                        victim:ChatPrint("PAP Upgrade: " .. upgradeDescriptions.explode)
+                    end)
                 end
-            end
-        end)
-    end)
-
-    self:AddHook("dn_module_fall", function(owner, victim)
-        if not owner.PAPDramaticDeathNote then return end
-        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/cartoon_fling_sound.mp3")
-        owner:ChatPrint("DeathNote upgrade: Played a cartoon fling noise")
-    end)
-
-    self:AddHook("dn_module_heartattack", function(owner, victim)
-        if not owner.PAPDramaticDeathNote then return end
-        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/vine_boom.mp3")
-        owner:ChatPrint("DeathNote upgrade: Played the vine boom")
-    end)
-
-    self:AddHook("dn_module_ignite", function(owner, victim)
-        if not owner.PAPDramaticDeathNote then return end
-        victim.PAPDramaticDeathNoteIgnite = true
-        owner:ChatPrint("DeathNote upgrade: Kills x5 as fast, starts fires after they die")
-    end)
-
-    self:AddHook("EntityTakeDamage", function(ent, dmg)
-        if ent.PAPDramaticDeathNoteIgnite and dmg:IsDamageType(DMG_BURN) then
-            dmg:ScaleDamage(5)
-        end
-    end)
-
-    self:AddHook("PostPlayerDeath", function(ply)
-        if ply.PAPDramaticDeathNoteIgnite then
-            local tr = util.QuickTrace(ply:GetPos(), Vector(0, 0, -1))
-            StartFires(ply:GetPos(), tr, 20, 40, false, ply)
-            ply.PAPDramaticDeathNoteIgnite = nil
-        end
-    end)
-
-    self:AddHook("dn_module_prematureburial", function(owner, victim)
-        if not owner.PAPDramaticDeathNote then return end
-        local timername = owner:SteamID64() .. "TTTPAPDramaticDeathNoteBurial"
-        victim:EmitSound("ttt_pack_a_punch/dramatic_death_note/lego_yoda.mp3")
-        owner:ChatPrint("DeathNote upgrade: Victim sunk way further into the ground, played the lego yoda death noise")
-
-        timer.Create(timername, 0.01, 500, function()
-            if GetRoundState() ~= ROUND_PREP and self:IsAlive(victim) then
-                local pos = victim:GetPos()
-                pos.z = pos.z - 1
-                victim:SetPos(pos)
-            else
-                timer.Remove(timername)
-
-                return
             end
         end)
     end)
