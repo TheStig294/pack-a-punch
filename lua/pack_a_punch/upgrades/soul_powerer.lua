c97b6fef0f49deb2a6ff836be5cffaa38dfbbe6e
thestig294 <thestig294@gmail.com>
2024-07-21 15:00:53 +0930

    Added a test upgraded Soulbound ability for new soulbinding device upgrade

--- lua/pack_a_punch/upgrades/soul_powerer.lua
+++ lua/pack_a_punch/upgrades/soul_powerer.lua
@@ -2,38 +2,42 @@ local UPGRADE = {}
 UPGRADE.id = "soul_powerer"
 UPGRADE.class = "weapon_ttt_smg_soulbinding"
 UPGRADE.name = "Soul Powerer"
+UPGRADE.desc = "All Soulbound abilities are upgraded!"
 
-UPGRADE.convars = {
-    {
-        name = "pap_soul_powerer_extra_abilities",
-        type = "int"
-    }
-}
+hook.Add("PostGamemodeLoaded", "TTTPAPSoulPowererPrepareAbilities", function()
+    if not SOULBOUND then return end
+    SOULBOUND.PAPAbilities = {}
 
-local extraAbilitiesCvar = CreateConVar("pap_soul_powerer_extra_abilities", "3", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Number of extra abilities", 1, 8)
-
-UPGRADE.desc = "The Soulbound gains " .. extraAbilitiesCvar:GetInt() .. " extra abilities!"
-local oldAbilityCount
-local maxAbilitiesCvar
+    for id, ABILITY in pairs(SOULBOUND.Abilities) do
+        if string.StartsWith(id, "pap_") then
+            SOULBOUND.PAPAbilities[id] = ABILITY
+            SOULBOUND.Abilities[id] = nil
+        end
+    end
+end)
 
 function UPGRADE:Apply(SWEP)
-    if CLIENT then return end
-    maxAbilitiesCvar = maxAbilitiesCvar or GetConVar("ttt_soulbound_max_abilities")
-
-    if not oldAbilityCount then
-        oldAbilityCount = maxAbilitiesCvar:GetInt()
+    -- Take a copy of the soulbound abilities to restore once we're done
+    if not SOULBOUND.PAPOldAbilities then
+        SOULBOUND.PAPOldAbilities = table.Copy(SOULBOUND.Abilities)
     end
 
-    maxAbilitiesCvar:SetInt(maxAbilitiesCvar:GetInt() + extraAbilitiesCvar:GetInt())
+    self:AddToHook(SWEP, "OnSuccess", function(ply, body)
+        print(ply, body)
+        ply.PAPSoulPowerer = true
+    end)
 end
 
 function UPGRADE:Reset()
-    if CLIENT then return end
-    maxAbilitiesCvar = maxAbilitiesCvar or GetConVar("ttt_soulbound_max_abilities")
+    -- Hopefully restore the Soulbound abilities to what they were
+    -- (I've had problems with this in the past with the French randomat restoring old role names to English,
+    -- but this is way less complicated than that so it will hopefully *just work*)
+    if SOULBOUND.PAPOldAbilities then
+        SOULBOUND.Abilities = table.Copy(SOULBOUND.PAPOldAbilities)
+    end
 
-    if oldAbilityCount then
-        maxAbilitiesCvar:SetInt(oldAbilityCount)
-        oldAbilityCount = nil
+    for _, ply in player.Iterator() do
+        ply.PAPSoulPowerer = nil
     end
 end
 
