27d3d9f2f4dd5b3b700d3c636c530f1e1381eacb
thestig294 <thestig294@gmail.com>
2024-08-23 16:32:00 +0930

    Added template upgrade

---
+++ lua/pack_a_punch/upgrades/`template.lua
@@ -0,0 +1,10 @@
+local UPGRADE = {}
+UPGRADE.id = "template"
+UPGRADE.class = "template"
+UPGRADE.name = "template"
+UPGRADE.desc = "template"
+
+function UPGRADE:Apply(SWEP)
+end
+
+TTTPAP:Register(UPGRADE)
