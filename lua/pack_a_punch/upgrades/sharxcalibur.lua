e89b97094104fb1b4238d7839b85bf90799ade40
thestig294 <thestig294@gmail.com>
2024-09-04 12:49:15 +0930

    fix lua error when opening buy menu

--- lua/pack_a_punch/upgrades/sharxcalibur.lua
+++ lua/pack_a_punch/upgrades/sharxcalibur.lua
@@ -7,6 +7,10 @@ UPGRADE.newClass = "weapon_sharxcalibur"
 UPGRADE.noCamo = true
 
 function UPGRADE:Condition(SWEP)
+    -- If we're just checking the buy menu icon, this weapon is always upgradeable
+    if not IsValid(SWEP) then return true end
+    -- If the weapon is bought and valid, make sure it isn't in the process of being used before letting it be upgraded
+    -- (Else it could be used twice, which typically PaP upgrades don't allow, unless that is the upgrade in of itself)
     local owner = SWEP:GetOwner()
 
     return not IsValid(owner) or not owner:GetNWBool("bonInv")
