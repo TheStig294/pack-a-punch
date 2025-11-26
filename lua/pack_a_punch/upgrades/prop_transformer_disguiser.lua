654d7970c35d5116f7bc6f4b7c35daac443eb481
The Stig <thestig294@gmail.com>
2025-11-26 16:46:37 +1030

    Added networking to prop disguiser upgrade

--- lua/pack_a_punch/upgrades/prop_transformer_disguiser.lua
+++ lua/pack_a_punch/upgrades/prop_transformer_disguiser.lua
@@ -10,76 +10,81 @@ function UPGRADE:Apply(SWEP)
         if IsValid(disguiser) and disguiser.TTTPAPPropTransformer then return false end
     end)
 
+    local function SetWeaponFunctions(wep, owner, victim)
+        print(wep, owner, victim)
+        if not IsValid(wep) or not IsValid(owner) or not IsValid(victim) then return end
+        local disguiser = victim:GetWeapon(UPGRADE.class)
+        print(disguiser)
+        if not IsValid(disguiser) then return end
+        disguiser:PrimaryAttack()
+
+        if SERVER then
+            victim:ChatPrint("Someone permanently turned you into a prop using the upgraded prop disguiser!")
+        end
+
+        disguiser.AllowDrop = false
+
+        function disguiser:SecondaryAttack()
+            if CLIENT then return end
+            self:GetOwner():PrintMessage(HUD_PRINTCENTER, "You're stuck as a prop, go hide!")
+        end
+
+        function disguiser:OnDrop()
+            if SERVER then
+                self:Remove()
+            end
+        end
+
+        function disguiser:ShouldDropOnDie()
+            return false
+        end
+
+        function disguiser:Holster()
+            return false
+        end
+
+        if SERVER then
+            wep:Remove()
+            owner:ConCommand("lastinv")
+        end
+    end
+
+    if SERVER then
+        util.AddNetworkString("TTTPAPPropTransformerDisguiser")
+    else
+        net.Receive("TTTPAPPropTransformerDisguiser", function()
+            local disguiser = net.ReadEntity()
+            local owner = net.ReadPlayer()
+            local victim = net.ReadPlayer()
+            SetWeaponFunctions(disguiser, owner, victim)
+        end)
+    end
+
     SWEP.PAPOldPrimaryAttack = SWEP.PrimaryAttack
 
     function SWEP:PrimaryAttack()
+        if CLIENT then return end
         local owner = self:GetOwner()
-
-        if SERVER and owner.LagCompensation then
-            owner:LagCompensation(true)
-        end
-
         local victim = owner:GetEyeTrace().Entity
-
-        if SERVER and owner.LagCompensation then
-            owner:LagCompensation(false)
-        end
-
         print(owner, victim)
 
         if UPGRADE:IsPlayer(victim) then
-            if SERVER then
-                victim:StripWeapons()
-                local disguiser = victim:Give(UPGRADE.class)
-                disguiser.TTTPAPPropTransformer = true
+            victim:StripWeapons()
+            local disguiser = victim:Give(UPGRADE.class)
+            disguiser.TTTPAPPropTransformer = true
 
-                function disguiser:Holster()
-                    return false
-                end
-
-                victim:SelectWeapon(UPGRADE.class)
-                owner:PrintMessage(HUD_PRINTCENTER, "Transforming " .. victim:Nick() .. " into a prop...")
+            function disguiser:Holster()
+                return false
             end
 
-            timer.Simple(2, function()
-                print(self, owner, victim)
-                if not IsValid(self) or not IsValid(owner) or not IsValid(victim) then return end
-                local disguiser = victim:GetWeapon(UPGRADE.class)
-                print(disguiser)
-                if not IsValid(disguiser) then return end
-                disguiser:PrimaryAttack()
-
-                if SERVER then
-                    victim:ChatPrint("Someone permanently turned you into a prop using the upgraded prop disguiser!")
-                    owner:PrintMessage(HUD_PRINTCENTER, victim:Nick() .. " transformed!")
-                end
-
-                disguiser.AllowDrop = false
-
-                function disguiser:SecondaryAttack()
-                    if CLIENT then return end
-                    self:GetOwner():PrintMessage(HUD_PRINTCENTER, "You're stuck as a prop, go hide!")
-                end
-
-                function disguiser:OnDrop()
-                    if SERVER then
-                        self:Remove()
-                    end
-                end
-
-                function disguiser:ShouldDropOnDie()
-                    return false
-                end
-
-                function disguiser:Holster()
-                    return false
-                end
-
-                if SERVER then
-                    self:Remove()
-                    owner:ConCommand("lastinv")
-                end
-            end)
+            victim:SelectWeapon(UPGRADE.class)
+            owner:PrintMessage(HUD_PRINTCENTER, "Transforming " .. victim:Nick() .. " into a prop...")
+            SetWeaponFunctions(self, owner, victim)
+            net.Start("TTTPAPPropTransformerDisguiser")
+            net.WriteEntity(self)
+            net.WritePlayer(owner)
+            net.WritePlayer(victim)
+            net.Broadcast()
         end
     end
 end
